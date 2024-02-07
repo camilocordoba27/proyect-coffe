f550ecf08fb9207ab72c463700c515340b14902a
camilocordoba27 <camilocordobaa@gmail.com>
2024-02-07 01:17:32 -0300

    navbar finished

--- cafe/src/components/navbar/navbar.jsx
+++ cafe/src/components/navbar/navbar.jsx
@@ -1,20 +1,58 @@
 import React from 'react'
+import Logo from "../../assets/website/coffee_logo.png"
+import {FaCoffee} from 'react-icons/fa'
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Services",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "About",
+    link: "/#about",
+  },
+];
 
 const NavBar = () => {
   return (
-    <div>
-        <div className="container bg-red-500">
-            <div className="flex">
-                {/* logo */}
-                <div>
-                    logo
-                </div>
-                {/* Links */}
-                <div>
+    <div className='bg-gradient-to-r from-secondary to-secondary/90 text-white'>
+      <div className="container py-2">
+        <div className="flex justify-between items-center gap-4">
+          {/* logo */}
+          <div className='flex'>
 
-                </div>
-            </div>
+            <a href="#" className='font-bold text-2xl sm:text-3xl flex justify-center items-center gap-2 tracking-wider font-cursive'>
+              <img src={Logo} alt="" className='w-14' />
+
+              Coffe Cafe
+            </a>
+          </div>
+          {/* Links */}
+          <div className='flex justify-between items-center gap-4'>
+             <ul className='hidden sm:flex items-center gap-4'>
+                 {
+                  Menu.map((data, index) => (
+                    <li key={data.link}>
+                      <a href="#" className='inline-block text-xl py-4 px-4 text-white/70 hover:text-white duration-200'>
+                        {data.name}
+                      </a>
+                    </li>
+                  ))}
+             </ul>
+             <button className='bg-primary/70 px-4 py-2 rounded-full hover:scale-105 duration-200 flex items-center gap-3'>
+              Order
+             <FaCoffee className='text-xl cursor-pointer'/>
+             </button>   
+          </div>
         </div>
+      </div>
     </div>
   )
 }
